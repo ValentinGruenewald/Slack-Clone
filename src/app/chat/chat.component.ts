031515ee13ac09406d8324c63a15020d25b41f4d
Valentin-Gruenewald <112621829+Valentin-Gruenewald@users.noreply.github.com>
2023-04-20 23:50:37 +0400

    chat messages now render from an array

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+  styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent {
-
+  allMessages = [
+    {
+      name: 'Name1',
+      message: 'Message1',
+      time: '17:59',
+    },
+    {
+      name: 'Name1',
+      message: 'Message2',
+      time: '18:00',
+    },
+    {
+      name: 'Name1',
+      message: 'Message3',
+      time: '18:05',
+    },
+  ];
 }
