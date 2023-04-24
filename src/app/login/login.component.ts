ac96b5080bd96d17d72dc992312c4e9f15810f88
Boubkir <109249622+Boubkir@users.noreply.github.com>
2023-04-24 17:10:23 +0200

    added google auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
-
-
+export class LoginComponent implements OnInit {
+  constructor(public authService: AuthService) {}
+  ngOnInit() {}
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { GoogleAuthProvider } from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  isLoggedIn = false;
 
-  constructor() { }
+  constructor(public firebaseAuth: AngularFireAuth) {}
+
+  async signin(email, password) {
+    await this.firebaseAuth
+      .signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        this.isLoggedIn = true;
+        localStorage.setItem('user', JSON.stringify(response.user));
+      });
+  }
+
+  async signup(email, password) {
+    await this.firebaseAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then((response) => {
+        this.isLoggedIn = true;
+        localStorage.setItem('user', JSON.stringify(response.user));
+      });
+  }
+
+  logOut() {
+    this.firebaseAuth.signOut();
+    localStorage.removeItem('user');
+  }
+
+  GoogleAuth() {
+    return this.AuthLogin(new GoogleAuthProvider());
+  }
+  // Auth logic to run auth providers
+  AuthLogin(provider) {
+    return this.firebaseAuth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log('You have been successfully logged in!');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
