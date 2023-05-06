313a476076edf30be7fe99cc44cfa7c029ad72ca
Boubkir <109249622+Boubkir@users.noreply.github.com>
2023-05-06 22:40:03 +0200

    changed google auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -13,6 +13,8 @@ import {
 } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { HotToastService } from '@ngneat/hot-toast';
+import { UserProfile } from 'src/models/user-profile';
+import { Firestore, doc, setDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
@@ -24,48 +26,54 @@ export class AuthService {
     public firebaseAuth: AngularFireAuth,
     private auth: Auth,
     public router: Router,
-    private toast: HotToastService
+    private toast: HotToastService,
+    private firestore: Firestore
   ) {}
 
   signUp(email, password) {
-    return from(createUserWithEmailAndPassword(this.auth, email, password))
+    return from(createUserWithEmailAndPassword(this.auth, email, password));
   }
 
   login(username, password) {
     return from(signInWithEmailAndPassword(this.auth, username, password));
   }
 
-logOut() {
-  this.auth.signOut().then(() => {
-    this.router.navigate(['']);
-    this.toast.success('You have been successfully logged out.');
-  });
-}
-
-GoogleAuth() {
-  return this.AuthLogin(new GoogleAuthProvider());
-}
-
-AuthLogin(provider) {
-  return this.firebaseAuth
-    .signInWithPopup(provider)
-    .then((result) => {
-      this.router.navigate(['/client']);
-      this.toast.success('You have been successfully logged in!');
-    })
-    .catch((error) => {
-      console.log(error);
-      this.toast.error('There was an error while logging in.');
+  logOut() {
+    this.auth.signOut().then(() => {
+      this.router.navigate(['']);
+      this.toast.success('You have been successfully logged out.');
     });
   }
 
-  updateProfile(profileData: Partial<UserInfo>): Observable<any> {
-    const user = this.auth.currentUser;
-    return of(user).pipe(
-      concatMap((user) => {
-        if (!user) throw new Error('You are not Authenticated');
-        return updateProfile(user,profileData)
+  GoogleAuth() {
+    return this.AuthLogin(new GoogleAuthProvider());
+  }
+
+  AuthLogin(provider) {
+    return this.firebaseAuth
+      .signInWithPopup(provider)
+      .then((result) => {
+        const user = result.user;
+        const userData = {
+          uid: user.uid,
+          displayName: user.displayName,
+          email: user.email,
+          photoURL: user.photoURL,
+        };
+        return this.addUser(userData);
       })
-    );
+      .then(() => {
+        this.router.navigate(['/client']);
+        this.toast.success('You have been successfully logged in!');
+      })
+      .catch((error) => {
+        console.log(error);
+        this.toast.error('There was an error while logging in.');
+      });
+  }
+
+  addUser(user: UserProfile): Observable<any> {
+    const ref = doc(this.firestore, 'users', user?.uid);
+    return from(setDoc(ref, user));
   }
 }
